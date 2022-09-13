a1eeed20ee22fa06d5951fd8cff37cd720a1c0b8
tom <tom.byttebier@protonmail.com>
2022-09-13 21:39:49 +0200

    15-2

--- 2015/2/code.py
+++ 2015/2/code.py
@@ -2,13 +2,32 @@
 # Author = ?
 # Date = December 2021
 
+from functools import partial
+import math
+
+
+def part1(input):
+    total = 0
+    for line in input.split('\n'):
+        l, w, h = map(int, line.split('x'))
+        total += 2*l*w + 2*w*h + 2*h*l + math.prod(sorted([l,w,h])[:2])
+
+    return total
+
+def part2(input):
+    total = 0
+    for line in input.split('\n'):
+        l, w, h = map(int, line.split('x'))
+        total += (l * w * h) + sum(map(lambda x:x*2, sorted([l,w,h])[:2]))
+    return total
+
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
 
 
-print("Part One : "+ str(None))
+print("Part One : "+ str(part1(input)))
 
 
 
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(part2(input)))
